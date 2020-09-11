e1827ca2238893d7c653d59b1143b3de943038ea
Jakub <kubplon@gmail.com>
2020-09-11 19:59:13 +0200

    Initial commit

---
+++ src/main/java/pl/red/rocket/fragments/HomeController.java
@@ -0,0 +1,12 @@
+package pl.red.rocket.fragments;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("index")
+    public String home() {
+        return "index";
+    }
+}
---
+++ src/main/java/pl/red/rocket/fragments/ThymeleafFragmentsApplication.java
@@ -0,0 +1,13 @@
+package pl.red.rocket.fragments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafFragmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafFragmentsApplication.class, args);
+	}
+
+}
